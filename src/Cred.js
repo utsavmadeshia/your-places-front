7da72eb335860fd3425db4843bbfd3df02f9b735
Utsav Madeshia <utsavmadeshia@gmail.com>
2023-08-21 20:46:01 +0530

    my changes

--- src/Cred.js
+++ src/Cred.js
@@ -1,2 +1,2 @@
  export const  REACT_APP_BACKEND_URL='https://yourplaces-rder.onrender.com/api'
- export const REACT_APP_ASSET_URL='https://yourplaces-rder.onrender.com/'
+ export const REACT_APP_ASSET_URL='https://yourplaces-rder.onrender.com'
